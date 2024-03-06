776c39f210c3e49a51074a6eb9c2b087d3f00fba
Maxruizdz <maxruizdz@hotmail.com>
2024-03-06 13:19:33 -0300

    Creacion de controlador y vista para registro

---
+++ Curso_Identity/Controllers/CuentasController.cs
@@ -0,0 +1,19 @@
+﻿using Curso_Identity.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curso_Identity.Controllers
+{
+    public class CuentasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Registro() { 
+        RegistroViewModel registroVM= new RegistroViewModel();
+        return View(registroVM);
+        }
+    }
+}
