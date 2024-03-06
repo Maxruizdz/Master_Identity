355c43ed4d0b0e3f2d0cab0d7c4954e34aedc051
Maxruizdz <maxruizdz@hotmail.com>
2024-03-06 10:00:44 -0300

    Agregando mas campos a la tabla Usuarios

---
+++ Curso_Identity/Models/AppUsuario.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Curso_Identity.Models
+{
+    public class AppUsuario : IdentityUser
+    {
+        public string Nombre { get; set; }
+        public string Url { get; set; }
+        public Int32 CodigoPais { get; set; }
+        public string telefono { get; set; }
+        public string Pais {get; set; }
+        public string Ciudad { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public bool Estado{ get; set; }
+
+
+
+
+
+    }
+}
