3a8ba96b9b5b55c9c43d2b3dc838ddf7fc6d9f4a
Maxruizdz <maxruizdz@hotmail.com>
2024-03-06 20:39:28 -0300

    Registrando usuarios en la base de datos

--- Curso_Identity/Controllers/CuentasController.cs
+++ Curso_Identity/Controllers/CuentasController.cs
@@ -1,10 +1,21 @@
-﻿using Curso_Identity.ViewModels;
+﻿using Curso_Identity.Models;
+using Curso_Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Curso_Identity.Controllers
 {
     public class CuentasController : Controller
     {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public CuentasController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,6 +25,40 @@ namespace Curso_Identity.Controllers
         public async Task<IActionResult> Registro() { 
         RegistroViewModel registroVM= new RegistroViewModel();
         return View(registroVM);
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Registro(RegistroViewModel vmRegistro) {
+
+            if (ModelState.IsValid) { 
+            
+             var
+                app_new_Usuari = new AppUsuario() {UserName= vmRegistro.Email,Nombre= vmRegistro.Nombre, Email= vmRegistro.Email, Ciudad= vmRegistro.Ciudad, CodigoPais=vmRegistro.CodigoPais, Pais= vmRegistro.Pais,Url= vmRegistro.Url , Direccion=vmRegistro.Direccion, FechaNacimiento= vmRegistro.FechaNacimiento, Estado= vmRegistro.Estado };
+             var resultado=await _userManager.CreateAsync(app_new_Usuari, vmRegistro.Password);
+
+                if (resultado.Succeeded)
+                {
+
+                    await _signInManager.SignInAsync(app_new_Usuari, isPersistent: false);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else { ValidarErrores(resultado); }
+            }
+
+
+            return View(vmRegistro);
+        }
+
+        private void ValidarErrores(IdentityResult resultado)
+        {
+
+            foreach (var error in resultado.Errors) {
+
+                ModelState.AddModelError(string.Empty, error.Description);
+            
+            
+            }
         }
     }
 }
