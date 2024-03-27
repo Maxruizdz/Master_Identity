09aae1358cae35953e53582ce7cdad734bd0d668
Maxruizdz <maxruizdz@hotmail.com>
2024-03-27 11:09:27 -0300

    Modificando el modelo Usuario

--- Curso_Identity/Models/AppUsuario.cs
+++ Curso_Identity/Models/AppUsuario.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Curso_Identity.Models
 {
@@ -9,13 +12,22 @@ namespace Curso_Identity.Models
         public string Url { get; set; }
         public Int32 CodigoPais { get; set; }
         public string telefono { get; set; }
-        public string Pais {get; set; }
+        public string Pais { get; set; }
         public string Ciudad { get; set; }
         public string Direccion { get; set; }
         public DateTime FechaNacimiento { get; set; }
-        public bool Estado{ get; set; }
+        public bool Estado { get; set; }
+
+        [NotMapped]
+        [Display(Name=("Rol para el usuario"))]
+        public string  IdRol { get; set; }
+
+        [NotMapped]
+        public string Rol { get; set; }
 
 
+        [NotMapped]
+        public IEnumerable<SelectListItem> ListaRoles { get; set; }
 
 
 
